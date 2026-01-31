fd28439671d7e107a68046165aca6fc910d22e13
Master <Master@DESKTOP-U0TSOM6>
2026-01-31 01:15:03 -0500

    Archivos principales del proyecto creados
    -Avanze en VistaMenu (muy poco)

--- src/main/java/vista/VistaMenu.java
+++ src/main/java/vista/VistaMenu.java
@@ -21,8 +21,7 @@ public class VistaMenu extends javax.swing.JFrame {
      */
     public VistaMenu() {
         initComponents();
-        //xdxd
-//Ajustar la imagen del logo de la EPN
+    //Ajustar la imagen del logo de la EPN
     java.awt.EventQueue.invokeLater(() -> {
         ImageIcon img = new ImageIcon(getClass().getResource("/img/politecnicaLogo.png"));
         Image escala = img.getImage().getScaledInstance(lblPoliLogo.getWidth(), 
